67f05f06863fec7628ae30cf5e610b710db2c3a8
StanislavKozel <s.kozel46@gmail.com>
2022-08-26 15:32:42 +0000

    add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-            alert("You clicked Submit!");
+            checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -30,16 +30,49 @@ function runGame(gameType) {
      if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
      } else {
-        alert ('Unknown game type: ${gameType}');
-        throw 'Uknown game type: ${gameType}.Aborting!';
+        alert (`Unknown game type: ${gameType}`);
+        throw `Uknown game type: ${gameType}.Aborting!`;
      }
 }
 
+
+/**
+ * check the answer against the first element in 
+ * hte returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
+   
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww..... you answered ${userAnswer}. the correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 
 }
 
+
+/**
+ * Gets the opernads (the numbers) and the operator (plus,minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`unimplimented operator ${operator}`);
+        throw `unimplimented operator ${operator}. Aborting!`;
+    }
 
 }
 
